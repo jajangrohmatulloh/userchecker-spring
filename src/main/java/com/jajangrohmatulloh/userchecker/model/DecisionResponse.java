6b11910915f1256a677e215c2565829bb152789e
jajangrohmatulloh <jajang.rohmatulloh02@gmail.com>
2024-09-19 17:13:59 +0700

    finished

--- src/main/java/com/jajangrohmatulloh/userchecker/model/DecisionResponse.java
+++ src/main/java/com/jajangrohmatulloh/userchecker/model/DecisionResponse.java
@@ -2,14 +2,11 @@ package com.jajangrohmatulloh.userchecker.model;
 
 import java.util.List;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class DecisionResponse {
     private List<String> decisions;
-
-    public List<String> getDecisions() {
-        return decisions;
-    }
-
-    public void setDecisions(List<String> decisions) {
-        this.decisions = decisions;
-    }
 }
